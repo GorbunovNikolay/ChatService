9aef1b4ae1f23b4fc4c70a3c50659f94e8e89658
Gorbunov Nikolay <gor_bunk@mail.ru>
2022-01-26 12:07:23 +0300

    ДЗ Выполнено

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    ChatService.createChat(1, 10)
+    ChatService.createChat(1, 5)
+    ChatService.createChat(2, 4)
+    ChatService.createChat(2, 1)
+    ChatService.createChat(3, 2)
+    ChatService.sendMessage(1,10, "Привет")
+    ChatService.sendMessage(1,5, "Как дела?")
+    ChatService.sendMessage(2,4, "Чем занимаешься?")
+    ChatService.sendMessage(3,2, "Выполняем ДЗ")
+    ChatService.getChats(2)
+    ChatService.deleteChat(3)
+    ChatService.deleteMessage(1)
+    ChatService.getCountUnreadChats(1)
+    ChatService.getMessage(1, 10, 2)
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
 }
